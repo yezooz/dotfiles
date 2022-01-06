ced7059c4a7acf1d2ce4953f100c0dff1844f330
Marek Mikuliszyn <marek.mikuliszyn@gmail.com>
2022-01-06 08:09:56 +0000

    cleanup

---
+++ linux.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Caps Lock as ESC for Vim
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:escape']"
+
+echo "Hello, Linux!"
---
+++ macos.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh"
+
+# echo "Hello, Mac!"
