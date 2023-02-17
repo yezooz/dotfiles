8f6c6413cf4ae10b6c06bdb0167cb064a43fbf3d
Marek Mikuliszyn <marek.mikuliszyn@gmail.com>
2023-02-17 19:20:44 +0000

    nodejs colours

--- zsh/p10k.zsh
+++ zsh/p10k.zsh
@@ -1052,8 +1052,8 @@
 
   ##############################[ node_version: node.js version ]###############################
   # Node version color.
-  typeset -g POWERLEVEL9K_NODE_VERSION_FOREGROUND=7
-  typeset -g POWERLEVEL9K_NODE_VERSION_BACKGROUND=2
+  typeset -g POWERLEVEL9K_NODE_VERSION_FOREGROUND=0
+  typeset -g POWERLEVEL9K_NODE_VERSION_BACKGROUND=5
   # Show node version only when in a directory tree containing package.json.
   typeset -g POWERLEVEL9K_NODE_VERSION_PROJECT_ONLY=true
   # Custom icon.
